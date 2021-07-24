c128091cb655835b42cb24e5f13196f8789d4cf7
Santiago Pozuelo <santiagopozuelo@hotmail.com>
2021-07-23 20:01:20 -0700

    changes

--- db/planService.js
+++ db/planService.js
@@ -1,17 +1,22 @@
 const {db}= require('./firebase.js')
 
-
+const userService = require("./userService.js")
+const planPeopleService = require("./planPeopleService.js")
 const PlansTable = process.env.PLANS_TABLE
 const UsersTable = process.env.USERS_TABLE
 const PlansPeople = process.env.PLANS_PEOPLE
+const {changeStatus} = planPeopleService
 
 
 
 async function createPlan(planData) {
+//return planId
+
     console.log(planData)
     const val = await db.collection(PlansTable).add(planData).then(plan=>{
         if (plan != null) {
             console.log(plan)
+            
             return plan
         }
 
@@ -19,6 +24,15 @@ async function createPlan(planData) {
 
     return val;
 }
+
+async function setUserHost(planId, userId) {
+
+    const response = await changeStatus(userId, planId, "host")
+    return response
+    
+
+}
+
 
 async function getPlanById(planId) {
     console.log(`plan id is: ${planId}`)
@@ -52,43 +66,6 @@ async function getUserRef(userId) {
 }
 
 
-async function changeStatus(planId, userId, status) {
-    console.log("inside changestatus")
-    const planRef = await getPlanRef(planId)
-    const memberSubref = await planRef.ref.collection(UsersTable).doc(userId)
-    const userRef = await getUserRef(userId)
-    const planSubref = await userRef.ref.collection(PlansTable).doc(planId)
-
-    const answer = await memberSubref.get().then(async snapshot=> {
-        if (snapshot.data()!=null && snapshot.data()["status"] == status) {
-            console.log("same status return false")
-            return false
-            //await memberSubref.set({status:"host"},{merge:true})
-        } else {
-            //maybe null, maybe other
-            console.log("diff status")
-            memberSubref.set({status: status},{merge: true})
-
-            var res = await planSubref.get().then(snap=>{
-                //if condition where status conflict
-                    //return false
-                planSubref.set({status: status},{merge:true})
-                console.log("setting planSubRef")
-                return true
-                
-            })
-            return res
-        }
-
-
-
-    })
-    console.log(`returning ${answer}`)
-    
-    return answer
-    //if (snapsho)
-
-}
 
 async function setUserDown(planId,userId) {
     const response = await changeStatus(planId,userId,"down")
@@ -99,72 +76,7 @@ async function setUserMaybe(planId,userId) {
     return response
 }
 
-async function setUserHost(planId, userId) {
 
-    const response = await changeStatus(planId, userId, "host")
-    return response
-    // const planRef = await getPlanRef(planId)
-    // const memberSubref = await planRef.ref.collection(UsersTable).doc(userId)
-
-
-    // const userRef = await getUserRef(userId)
-    // const planSubref = await userRef.ref.collection(PlansTable).doc(planId)
-    //var changedStatus = 
-
-
-    // const answer = memberSubref.get().then(async snapshot=> {
-
-    //     if (snapshot.data() != null) {
-    //         if (snapshot.data()["status"] != "host" ) {
-    //             //change status in other ref
-    //             memberSubref.set({status: "host"},{merge: true})
-    //             console.log("setting member subref")
-                
-    //             var ans = await planSubref.get().then(snap=> {
-    //                 if (snap.data() == null || snap.data()["status"]!= "host") {
-    //                     planSubref.set({status: "host"},{merge:true})
-    //                     console.log("setting planSubRef")
-    //                     return true
-    //                 }
-    //                 return false
-    //             })
-    //         } else {
-    //             return false
-    //         }
-            
-    //     } else {
-    //         await memberSubref.set({status:"host"},{merge:true})
-    //         var ans = await planSubref.get().then(snap=> {
-    //             if (snap.data() == null || snap.data()["status"]!= "host") {
-    //                 planSubref.set({status: "host"},{merge:true})
-    //                 console.log("setting planSubRef2")
-    //                 return true
-    //             } else {
-    //                 return false
-    //             }
-    //         })
-    //         return ans
-            
-    //         //change status in other ref
-    //     }
-
-    // })
-    // return answer
-
-
-    // const info = {
-    //     userId: userId,
-    //     planId: planRelation.id,
-    //     status: "host",
-    //     seen: "true"
-    // }
-
-    // var ans = await this.duplicateMembership(info)
-    // console.log(info)
-    // return ans
-    
-
-}
 
 async function duplicateMembership(info) {
     const planRelationRef = await db.collection(PlansPeople).add(info)
@@ -198,12 +110,5 @@ async function getListPlans(listIds) {
     return content
 }
 
-// async function getPlanById(planId) {
-//     db.collection(plansTable).doc(planId).get().then(doc=> {
-//         if (doc.exists && doc.data()!= null) {
-//             return doc.data()
-//         }
-//     })
-// }
 
-module.exports = {getPlanById, createPlan, getListPlans, setUserHost, setUserDown, setUserMaybe,setUserDown,changeStatus}
+module.exports = { getPlanById, createPlan, getListPlans, setUserHost, setUserDown, setUserMaybe,setUserDown,changeStatus}
